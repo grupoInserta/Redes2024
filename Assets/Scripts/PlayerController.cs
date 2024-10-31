6a6dba61ff39c7ac89cef52a049e22a106b14925
JaimeSevilla <jaimesevillamoreno1@gmail.com>
2024-10-31 19:05:12 +0100

    Cambios en el script de controlador de personajr para la rotacion del mismo No Funciona

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,21 @@ public class PlayerController : MonoBehaviour
     //Creamos la variable del player donde le asignamos el player que vamos a usar
     [SerializeField] GameObject player;
     //Accedemos al player mediante el transform del mismo para poder mover al personaje
-    private float vertical = 0;
-    private float horizontal = 0;
+    [SerializeField] private float vertical;
+    [SerializeField] private float horizontal;
 
     private float mouseVertical = 0;
     private float mouseHorizontal = 0;
 
     Vector3 inputValues = Vector3.zero;
 
-    [SerializeField] float rotationSmoothTime;
+    [SerializeField] float rotationSmoothTime = 0.2f;
     float currentAngle;
     float currentAngleVelocity;
 
     PlayerController controller;
     Camera cam;
-    
+
 
     private void Awake()
     {
@@ -31,41 +31,38 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-       
-
-        mouseVertical = Input.GetAxis("Mouse X");
-        mouseHorizontal = Input.GetAxis("Mouse Y");
-        //Vector2 MouseY += mouseVertical;
-        //Vector2 MouseX += mouseHorizontal;
-
-
         //El personaje tiene que rotar con respecto a la camara
-
-        float targetAngel = Mathf.Atan2(inputValues.x, inputValues.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
-
-        
-    }
-
-    private void HandleMovement()
-    {
-        
         vertical = Input.GetAxis("Vertical");
         horizontal = Input.GetAxis("Horizontal");
 
-        inputValues.z += vertical;
-        inputValues.x += horizontal;
+        inputValues.z = vertical;
+        inputValues.x = horizontal;
 
         player.transform.position += inputValues;
+        
+        HandleMovement();
 
+        //mouseVertical = Input.GetAxis("Mouse X");
+        // mouseHorizontal = Input.GetAxis("Mouse Y");
+        //Vector2 MouseY += mouseVertical;
+        //Vector2 MouseX += mouseHorizontal;
 
-        if(inputValues.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(inputValues.x, inputValues.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
-            currentAngle = Mathf.SmoothDamp(currentAngle, targetAngle, ref currentAngleVelocity, rotationSmoothTime);
-            transform.rotation = Quaternion.Euler(0, currentAngle, 0);
-
-            Vector3 rotatedMovement = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-            
-        }
+        
     }
+
+    
+    private void HandleMovement()
+    {
+        float targetAngle = Mathf.Atan2(inputValues.x, inputValues.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+        currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentAngleVelocity, rotationSmoothTime);
+        transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+
+
+        //float rotation = Mathf.Atan2(inputValues.x, inputValues.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+        //transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref currentAngleVelocity, rotationSmoothTime);
+
+        Debug.Log(transform.rotation);
+    }
+    
 }
+
